fe3b58b65dc800ff10b1894d3b3a8c06c4ed489b
kgd3443 <shyshy7665@naver.com>
2025-11-30 05:22:39 +0900

    완성본

--- src/components/NewsPage.tsx
+++ src/components/NewsPage.tsx
@@ -2,14 +2,18 @@ import { useParams } from "react-router-dom";
 import Categories from "./Categories";
 import NewsList from "./NewsList";
 
+type RouteParams = {
+  category?: string;
+};
+
 export default function NewsPage() {
-  const params = useParams();
-  const categoryFromUrl = params.category ?? "all";
+  const params = useParams<RouteParams>();
+  const category = params.category ?? "all";
 
   return (
     <>
       <Categories />
-      <NewsList category={categoryFromUrl} />
+      <NewsList category={category} />
     </>
   );
 }
